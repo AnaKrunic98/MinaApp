6612cc7451d58f72ec4d9e37005727452d5ae95d
Alen Ankajcan <alen.ankajcan@gmail.com>
2023-03-29 23:40:21 +0200

    Add firebase package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    modules: [
+        'nuxt-vuefire',
+    ],
+    vuefire: {
+        config: {
+            apiKey: 'AIzaSyDGypIG7ZRI8SX0QvwRWzWHpciwId8QkQ8',
+            authDomain: 'minaapp-84603.firebaseapp.com',
+            projectId: 'minaapp-84603',
+            storageBucket: 'minaapp-84603.appspot.com',
+            messagingSenderId: '346951131767',
+            appId: '1:346951131767:web:fe3906ad6588cbaba89c72',
+        },
+    },
 })
