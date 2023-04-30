d0e5bb5ae647bacad03ccab9fd233c51ba61af25
AnaKrunic98 <akrunic998@gmail.com>
2023-04-30 19:04:30 +0200

    Cleaned up code in MoodForm

--- pages/MoodForm.vue
+++ pages/MoodForm.vue
@@ -9,31 +9,32 @@
           <div class="mb-4 mt-10">
             <label class="block text-sm font-medium text-gray-900 " for="inputName">Ime:</label>
             <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-            id="inputName" placeholder="Upisi ime" v-model="form.name">
+            id="inputName" placeholder="Upisi ime" 
+            v-model="form.name">
           </div>
           <div class="mb-4">
             <label class="block text-sm font-medium text-gray-900" for="inputEmail">Email adresa:</label>
             <input type="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-            id="inputEmail" placeholder="Upisi email" v-model="form.email">
+            id="inputEmail" placeholder="Upisi email" 
+            v-model="form.email">
           </div>
       
           <div>
             <p>Raspoloženje:</p>
             <button 
               v-for="mood in moods" 
-              @click.prevent="setMood(mood.value)"
-              class="block focus:outline-none text-white bg-blue-300 hover:bg-blue-400 focus:ring-4 focus:ring-blue-400 font-medium rounded-lg text-sm 
-              px-5 py-2.5 mr-2 mb-2 dark:focus:ring-blue-600"
-              :class="{'active': form.mood === mood.value }" 
+              @click.prevent="setMood(mood)"
+              class="block text-white bg-blue-300 hover:bg-blue-500 focus:bg-blue-700 font-medium rounded-lg text-sm 
+              px-5 py-2.5 mr-2 mb-2"
+              :class="{'active': mood.selected }" 
               >{{ mood.name }}
             </button>
           </div>
 
           <button type="submit" 
-            @click="activeMood = !activeMood"
             class=" mt-6 focus:outline-none text-white bg-blue-400 hover:bg-blue-500 focus:ring-4 focus:ring-blue-600 font-medium rounded-lg text-sm 
             px-5 py-2.5 mr-2 mb-2 dark:focus:ring-blue-600">
-              Submit
+            Submit
           </button>
         </div>
       </form>
@@ -52,14 +53,17 @@ export default {
               {
                 name: "Sreća",
                 value: "happiness",
+                selected: false,
               },
               {
                 name: "Tuga",
                 value: "sadness",
+                selected: false,
               },
               {
                 name: "Ljutnja",
                 value: "anger",
+                selected: false,
               },
              ],
            form: {
@@ -80,7 +84,8 @@ export default {
         await this.handleMoodStore();
        },
        setMood(mood) {
-        this.form.mood = mood;
+        this.form.mood = mood.value;
+        mood.selected = !mood.selected;
       },
        async handleMoodStore() {
         console.log("dosao sam ovde"); 
@@ -105,7 +110,8 @@ export default {
 
 <style>
 .active {
-  background-color: rgb(96 165 250);
-  border: solid 10px rgb(37 99 235); 
+  background-color: rgb(29 78 216);
+  
 }
+
 </style>
