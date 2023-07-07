fadd18f274d3624dbc37e5133421d2d5c2c943ce
AnaKrunic98 <akrunic998@gmail.com>
2023-07-07 12:46:25 +0200

    login update

--- pages/Login.vue
+++ pages/Login.vue
@@ -1,45 +1,45 @@
- <template>
-    <div class=" bg-blue-100 min-h-screen">
-      <div class="flex justify-center py-10 font-medium">
-        <span>Login</span>
-      </div>
-      <div class="md:max-w-xl md:m-auto">
-      <form @submit.prevent="login">
-        <div class="grid gap-2 p-4 ">
-            <div class="mb-4 mt-10">
-                <label class="block text-sm font-medium text-gray-900 ">
-                    <input type="text" placeholder="Email address" v-model="form.email" 
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" >
-                </label>
-            </div>
-            <div class="mb-4">
-                <label class="block text-sm font-medium text-gray-900 ">
-                    <input type="password " placeholder="Password" v-model="form.password" 
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                </label>
-            </div>
-                <button type="submit" class=" mt-6 focus:outline-none text-white bg-blue-400 hover:bg-blue-500 focus:ring-4 focus:ring-blue-600 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-blue-600">
-                  <span>Log in</span>
-                </button>
-        </div>
-        <div v-if="errors.length>0">
-          <ul class="mx-5">
-            <li v-for="error in errors">{{ error }}</li>
-          </ul>
-        </div> 
-      </form>
-      <NuxtLink to="/signup" class="mx-5 hover:text-blue-700">You don't have an account</NuxtLink>
-
+<template>
+  <div class=" bg-blue-100 min-h-screen">
+    <div class="flex justify-center py-10 font-medium">
+      <span>Login</span>
     </div>
-    </div>    
+    <div class="md:max-w-xl md:m-auto">
+    <form @submit.prevent="login">
+      <div class="grid gap-2 p-4 ">
+          <div class="mb-4 mt-10">
+              <label class="block text-sm font-medium text-gray-900 ">
+                  <input type="text" placeholder="Email address" v-model="form.email" 
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" >
+              </label>
+          </div>
+          <div class="mb-4">
+              <label class="block text-sm font-medium text-gray-900 ">
+                  <input type="password " placeholder="Password" v-model="form.password" 
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+              </label>
+          </div> 
+              <button type="submit" class=" mt-6 focus:outline-none text-white bg-blue-400 hover:bg-blue-500 focus:ring-4 focus:ring-blue-600 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-blue-600">
+                <span>Log in</span>
+              </button>
+      </div>
+      <div v-if="errors"> 
+        <ul class="mx-5">
+          <li v-for="error in errors">{{ error }}</li>
+        </ul>
+      </div> 
+    </form>
+    <NuxtLink to="/signup" class="mx-5 hover:text-blue-700">You don't have an account</NuxtLink>
+
+  </div>
+  </div>    
 </template>
 
 <script setup>
 import { createClient } from '@supabase/supabase-js';
 
 const form = {
-  email:'',
-  password:'' ,
+email:'',
+password:'' ,
 };
 const errors = [];
 
@@ -49,43 +49,39 @@ const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 const validateForm = () => {
-  //resetuje niz, da se ne bi samo dodavale greske  
-  errors.length = 0 
-  let validated = true;
+//resetuje niz, da se ne bi samo dodavale greske  
+errors.length = 0 
+let validated = true;
 
-  if (!form.email) {
-    errors.push('Email address is required');
-    validated = false;
-  }
+if (!form.email) {
+  errors.push('Email address is required');
+  validated = false;
+}
 
-  if (!form.password) {
-    errors.push('Password is required');
-    validated = false;
-  }
+if (!form.password) {
+  errors.push('Password is required');
+  validated = false;
+}
 
-  return { validated, errors };
+return { validated, errors };
 };
 
 const login = async () => {
-  const { validated, errors } = validateForm();
+const { validated, errors } = validateForm();
 
-  if (validated) {
-    const {data, error} = await supabase.auth.signInWithPassword({
-    email: form.email,
-    password: form.password
-    })
-    console.log('data', data)
-    console.log('error', error)
-    console.log(form)
+if (validated) {
+  const {data, error} = await supabase.auth.signInWithPassword({
+  email: form.email,
+  password: form.password
+  })
+  console.log('data', data)
+  console.log('error', error)
+  console.log(form)
 
-    await navigateTo('/MoodForm');
-  }else {
-    console.log('Validation errors:', errors);
-  }
+  await navigateTo('/MoodForm');
+}else {
+  console.log('Validation errors:', errors);
+}
 }
 
 </script>
-
-<style>
-
-</style> 
